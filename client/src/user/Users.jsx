b83159ea273774195f54073fd790e33a7ad5ec9e
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-09 21:10:14 +0300

    update

--- client/src/user/Users.jsx
+++ client/src/user/Users.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Users() {
+  const [users, setUsers] = useState([
+    {
+      Name: 'Ahmed',
+      Email: 'ahmed@gmail.com',
+      Age: 20,
+    },
+  ]);
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -13,7 +20,15 @@ function Users() {
               <th>Action</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {users.map((user) => {
+              <tr>
+                <td>{user.Name}</td>
+                <td>{user.Email}</td>
+                <td>{user.Age}</td>
+              </tr>;
+            })}
+          </tbody>
         </table>
       </div>
     </div>
