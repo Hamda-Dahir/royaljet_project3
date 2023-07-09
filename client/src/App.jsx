38290ff9d71b1958b68bde6f909f34b7b5a7b5cb
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-09 20:54:33 +0300

    feat: added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Users from './user/Users';
+import CreateUser from './user/CreateUser';
+import UpdateUser from './user/UpdateUser';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUser />}></Route>
+          <Route path="/update" element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
