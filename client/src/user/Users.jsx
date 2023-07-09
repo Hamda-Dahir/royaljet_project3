d3c23b1288ea520179b42adb74f397e39e8342fb
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-09 20:59:56 +0300

    feat: added some style for users

--- client/src/user/Users.jsx
+++ client/src/user/Users.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function Users() {
-  return <div>Users</div>;
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Users;
