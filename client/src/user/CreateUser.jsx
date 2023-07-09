8e6d418644164f3aef24e1b2c24f1d98c92a3088
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-09 21:39:35 +0300

    feat: added create

--- client/src/user/CreateUser.jsx
+++ client/src/user/CreateUser.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function CreateUser() {
-  return <div>CreateUser</div>;
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form>
+          <h2>Add User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              className="form-control"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default CreateUser;
