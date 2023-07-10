a22792b16b9cf479de68fbda1176354d3181cbaa
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-10 14:10:45 +0300

    update

--- client/src/user/CreateUser.jsx
+++ client/src/user/CreateUser.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function CreateUser() {
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [age, setAge] = useState();
 
+  const Submit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3001/createUser', { name, email, age })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <form>
+        <form onSubmit={Submit}>
           <h2>Add User</h2>
           <div className="mb-2">
             <label htmlFor="">Name</label>
