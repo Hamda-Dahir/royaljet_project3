01a0a31e5a77cf1a5292ee2ed64bfa3d932c394f
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-10 16:58:42 +0300

    fix: id fetching detail page

--- client/src/user/UpdateUser.jsx
+++ client/src/user/UpdateUser.jsx
@@ -11,15 +11,31 @@ function UpdateUser() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3001/getUser' + id)
-      .then((result) => console.log(result))
+      .get('http://localhost:3001/getUser/' + id)
+      .then((result) => {
+        console.log(result);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setAge(result.data.age);
+      })
       .catch((err) => console.log(err));
   }, []);
+
+  const Update = (e) => {
+    e.preventDefault();
+    axios
+      .put('http://localhost:3001/updateUser/' + id, { name, email, age })
+      .then((result) => {
+        console.log(result);
+        navigate('/');
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
-        <form>
+        <form onSubmit={Update}>
           <h2>Update User</h2>
           <div className="mb-2">
             <label htmlFor="">Name</label>
@@ -27,6 +43,8 @@ function UpdateUser() {
               type="text"
               placeholder="Enter Name"
               className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -35,6 +53,8 @@ function UpdateUser() {
               type="email"
               placeholder="Enter Email"
               className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-2">
@@ -43,6 +63,8 @@ function UpdateUser() {
               type="text"
               placeholder="Enter Age"
               className="form-control"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
             />
           </div>
           <button className="btn btn-success">Update</button>
