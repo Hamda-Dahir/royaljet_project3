29cb17947483387f8d2c4dfb8227924f7b9be329
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-10 17:34:43 +0300

    fix: update usere

--- client/src/user/Users.jsx
+++ client/src/user/Users.jsx
@@ -12,6 +12,16 @@ function Users() {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleDelete = (id) => {
+    axios
+      .delete('http://localhost:3001/deleteUser/' + id)
+      .then((res) => {
+        console.log(res);
+        window.location.reload();
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
       <div className="w-50 bg-white rounded p-3">
@@ -41,9 +51,12 @@ function Users() {
                     >
                       Update
                     </Link>
-                    <Link to={'/'} className="btn btn-danger">
+                    <button
+                      className="btn btn-danger"
+                      onClick={(e) => handleDelete(user._id)}
+                    >
                       Delete
-                    </Link>
+                    </button>
                   </td>
                 </tr>
               );
