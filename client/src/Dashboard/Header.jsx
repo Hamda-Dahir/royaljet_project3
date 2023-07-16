caffc28308a31a56846b44ca92bdb70920bd49ab
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-16 12:07:03 +0300

    update

--- client/src/Dashboard/Header.jsx
+++ client/src/Dashboard/Header.jsx
@@ -1,14 +1,46 @@
 // Header.js
+import { left } from '@popperjs/core';
 import React from 'react';
 
 const Header = ({ userName }) => {
+  const handleLogout = () => {
+    // Implement your logout logic here
+    // For example, clearing the user session or state
+    console.log('Logged out');
+  };
+
   return (
     <header
       className="header"
-      style={{ backgroundColor: '#333', color: '#fff', padding: '10px' }}
+      style={{
+        backgroundColor: '#333',
+        color: '#fff',
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
     >
-      <h1>Dashboard Header</h1>
-      <p>Welcome, {userName}</p>
+      <div
+        style={{
+          marginLeft: '500px',
+        }}
+      >
+        <h3 style={{ margin: 0 }}>Royal Jet Management System</h3>
+        <p style={{ margin: '3px 0' }}>Welcome, {userName}</p>
+      </div>
+      <button
+        style={{
+          backgroundColor: '#555',
+          color: '#fff',
+          border: 'none',
+          padding: '8px 16px',
+          borderRadius: '4px',
+        }}
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </header>
   );
 };
