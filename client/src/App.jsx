5a4e184b80de85d42c4f2a2dd9d31b915e8c93c9
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-16 11:54:06 +0300

    update all responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -15,16 +16,18 @@ function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
-        <Header userName={userName} /> {/* Pass the userName prop */}
-        <div className="sidebar">
-          <SidebarMenu />
-        </div>
-        <div className="content">
-          <Routes>
-            <Route path="/users" element={<Users />}></Route>
-            <Route path="/create" element={<CreateUser />}></Route>
-            <Route path="/update/:id" element={<UpdateUser />}></Route>
-          </Routes>
+        <Header userName={userName} />
+        <div className="middle-section">
+          <div className="sidebar">
+            <SidebarMenu />
+          </div>
+          <div className="content">
+            <Routes>
+              <Route path="/users" element={<Users />}></Route>
+              <Route path="/create" element={<CreateUser />}></Route>
+              <Route path="/update/:id" element={<UpdateUser />}></Route>
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
