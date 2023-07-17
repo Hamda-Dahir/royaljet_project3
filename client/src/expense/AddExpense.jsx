f73a6738f81f65e600af8b5f70912c5cbcdd50ba
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-17 19:09:09 +0300

    feat: added add exp form

--- client/src/expense/AddExpense.jsx
+++ client/src/expense/AddExpense.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createExpense } from '../api';
+import { Form, Button } from 'react-bootstrap';
 
-const AddExpense = () => {
-  return <div>AddExpense</div>;
+const AddExpenseForm = ({ onClose }) => {
+  const navigate = useNavigate();
+  const [newExpenseData, setNewExpenseData] = useState({
+    description: '',
+    amount: 0,
+    date: '',
+    category: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { description, value } = event.target;
+    setNewExpenseData((prevExpenseData) => ({
+      ...prevExpenseData,
+      [description]: value,
+    }));
+  };
+
+  const handleAddExpense = async () => {
+    try {
+      await createExpense(newExpenseData);
+      onClose(); // Close the modal after adding a Expense
+      navigate('/expenses');
+    } catch (error) {
+      console.error('Error creating expenses:', error);
+    }
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="formDescription">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          type="text"
+          name="description"
+          value={newExpenseData.description}
+          placeholder="Enter description"
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formAmount">
+        <Form.Label>Amount</Form.Label>
+        <Form.Control
+          type="number"
+          name="amount"
+          value={newExpenseData.amount}
+          placeholder="Enter price"
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="date"
+          value={newExpenseData.date}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formCategory">
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          type="text"
+          name="category"
+          value={newExpenseData.category}
+          placeholder="Enter Category"
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" onClick={handleAddExpense}>
+        Add Expense
+      </Button>
+    </Form>
+  );
 };
 
-export default AddExpense;
+export default AddExpenseForm;
