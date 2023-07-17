eca9fa1451286f382d64a5635fb80511b90bb5b8
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-17 17:28:28 +0300

    feat: added expenses router structure

---
+++ server/routes/expenses.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ExpensesModel = require('../models/Expenses.js');
+
+const router = express.Router();
+
+// Create a new expenses
+
+// Get all expenses
+
+// Get a single expenses by ID
+
+// Update a expenses by ID
+
+// Delete a expenses by ID
+
+module.exports = router;
