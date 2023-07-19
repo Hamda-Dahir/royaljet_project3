897bbf8a2c003e2cc79b90e7a6621fe8ba779109
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-19 12:03:02 +0300

    feat: added delete route

--- server/routes/order.js
+++ server/routes/order.js
@@ -54,5 +54,17 @@ router.put('/:id', async (req, res) => {
 });
 
 // delete a order by id
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedOrder = await OrderModel.findByIdAndUpdate(id);
+    if (!deletedOrder) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.json(deletedOrder);
+  } catch (error) {
+    res.status(200).json({ message: 'Error deleting order', order });
+  }
+});
 
 module.exports = router;
