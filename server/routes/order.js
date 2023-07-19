32f6369c9cbf54ed889364dd361f20b4fab45d7d
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-19 11:09:00 +0300

    fea: added order router structure

---
+++ server/routes/order.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const OrderModel = require('../models/Order.js');
+
+const router = express.Router();
+
+// create a new order
+
+// get all orders
+
+// get a single order
+
+// update a order by id
+
+// delete a order by id
+
+module.exports = router;
