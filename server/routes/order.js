76c946905ca273d60ff47d3041536c063ec8f2e0
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-19 11:12:39 +0300

    feat: added create order route

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,6 +4,14 @@ const OrderModel = require('../models/Order.js');
 const router = express.Router();
 
 // create a new order
+router.post('/', async (req, res) => {
+  try {
+    const newOrder = await OrderModel.create(req.body);
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating order', error });
+  }
+});
 
 // get all orders
 
