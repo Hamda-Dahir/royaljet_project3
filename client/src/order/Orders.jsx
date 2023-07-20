4287b4617e7053337ba2fa622ab6d52eb18eb681
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-20 18:03:10 +0300

    added some fetchin data

--- client/src/order/Orders.jsx
+++ client/src/order/Orders.jsx
@@ -9,8 +9,39 @@ import AddOrderForm from './AddOrder';
 import UpdateOrderForm from './UpdateOrderForm';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-const Orders = () => {
+function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [showAddOrderModal, setShowAddOrderModal] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ordersPerPage] = useState(8);
+  const [filterName, setFilterName] = useState('');
+  const [newOrderData, setNewOrderData] = useState({
+    fullName: '',
+    phone: 0,
+    date: '',
+    details: '',
+    category: '',
+    price: '',
+    paymentType: '',
+  });
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const data = await getAllOrders();
+      setOrders(data);
+    } catch (error) {
+      console.error('Error fetching orders:', error);
+    }
+  };
+
   return <div>Orders</div>;
-};
+}
 
 export default Orders;
