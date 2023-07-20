c49cc76a06736314eb2110aab85e8f838d03c5a9
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-20 17:44:50 +0300

    feat: added import files in orders list

--- client/src/order/Orders.jsx
+++ client/src/order/Orders.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { FaFilter, FaFileAlt } from 'react-icons/fa';
+import OrderReport from './OrderReport';
+import './orders.css';
+import { getAllOrders, createOrder, updateOrder, deleteOrder } from '../api';
+import AddOrderForm from './AddOrder';
+import UpdateOrderForm from './UpdateOrderForm';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 const Orders = () => {
   return <div>Orders</div>;
