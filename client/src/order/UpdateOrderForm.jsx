912235caec2be7836d83a0addbd530080e96c450
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-22 08:32:28 +0300

    added form update order

--- client/src/order/UpdateOrderForm.jsx
+++ client/src/order/UpdateOrderForm.jsx
@@ -31,7 +31,84 @@ const UpdateOrderForm = ({ order, onClose, onUpdate }) => {
     }
   };
 
-  return <div>UpdateOrderForm</div>;
+  return (
+    <Form>
+      <Form.Group controlId="formName">
+        <Form.Label>FullName</Form.Label>
+        <Form.Control
+          type="text"
+          name="fullname"
+          value={updatedOrderData.fullname}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formPhone">
+        <Form.Label>Phone</Form.Label>
+        <Form.Control
+          type="number"
+          name="phone"
+          value={updatedOrderData.phone}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="date"
+          value={updatedOrderData.dadte}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formDetails">
+        <Form.Label>Detail</Form.Label>
+        <Form.Control
+          type="text"
+          name="details"
+          value={updatedOrderData.details}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formCategory">
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          type="text"
+          name="category"
+          value={updatedOrderData.category}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formPrice">
+        <Form.Label>Price</Form.Label>
+        <Form.Control
+          type="number"
+          name="price"
+          value={updatedOrderData.price}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formPayment">
+        <Form.Label>PaymentType</Form.Label>
+        <Form.Control
+          type="text"
+          name="paymentType"
+          value={updatedOrderData.paymentType}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" onClick={handleUpdateOrder}>
+        Update Order
+      </Button>
+    </Form>
+  );
 };
 
 export default UpdateOrderForm;
