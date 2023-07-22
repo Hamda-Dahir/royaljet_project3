eda298e31beeecf3579051c49105836aed7c5fd8
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-22 08:23:57 +0300

    update

--- client/src/order/UpdateOrderForm.jsx
+++ client/src/order/UpdateOrderForm.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { updateOrder } from '../api';
+import { Form, Button } from 'react-bootstrap';
 
-const UpdateOrderForm = () => {
+const UpdateOrderForm = ({ order, onClose, onUpdate }) => {
+  const [updatedOrderData, setUpdatedOrderData] = useState({
+    fullname: order.fullname,
+    phone: order.phone,
+    date: order.date,
+    details: order.details,
+    category: order.category,
+    price: order.price,
+    paymentType: order.paymentType,
+  });
   return <div>UpdateOrderForm</div>;
 };
 
