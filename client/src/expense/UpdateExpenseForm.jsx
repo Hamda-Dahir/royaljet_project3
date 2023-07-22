1e30188e9661b362a86e619eef6af6a44b1078d3
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-22 08:46:28 +0300

    fea: added update form

--- client/src/expense/UpdateExpenseForm.jsx
+++ client/src/expense/UpdateExpenseForm.jsx
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { updateExpense } from '../api';
+import { Form, Button } from 'react-bootstrap';
 
-const UpdateExpenseForm = () => {
-  return <div>UpdateExpenseForm</div>;
+const UpdateExpenseForm = ({ expense, onClose, onUpdate }) => {
+  const [updatedExpenseData, setUpdatedExpenseData] = useState({
+    description: expense.description,
+    amount: expense.amount,
+    date: expense.date,
+    category: expense.category,
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUpdatedExpenseData((prevExpenseData) => ({
+      ...prevExpenseData,
+      [name]: value,
+    }));
+  };
+
+  const handleUpdateExpense = async () => {
+    try {
+      const updatedExpense = await updateExpense(
+        expense._id,
+        updatedExpenseData
+      );
+      onUpdate(updatedExpense);
+      onClose();
+    } catch (error) {
+      console.error('Error updating Expense:', error);
+    }
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="formDesc">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          type="text"
+          name="description"
+          value={updatedExpenseData.description}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formAmount">
+        <Form.Label>Amount</Form.Label>
+        <Form.Control
+          type="number"
+          name="amount"
+          value={updatedExpenseData.amount}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control
+          type="date"
+          name="date"
+          value={updatedExpenseData.date}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group controlId="formCategory">
+        <Form.Label>Category</Form.Label>
+        <Form.Control
+          type="text"
+          name="category"
+          value={updatedExpenseData.category}
+          onChange={handleInputChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" onClick={handleUpdateExpense}>
+        Update Expense
+      </Button>
+    </Form>
+  );
 };
 
 export default UpdateExpenseForm;
