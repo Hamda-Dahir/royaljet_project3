54162eaed36c9c070fb13088b1477a57271c95cd
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-23 13:57:08 +0300

    update

--- client/src/order/OrderInvoiceModal.jsx
+++ client/src/order/OrderInvoiceModal.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const OrderInvoiceModal = ({ order, onClose }) => {
+  useEffect(() => {
+    // After the modal is mounted, wait for a short delay (e.g., 500ms) and then trigger print
+    const printTimeout = setTimeout(() => {
+      window.print(); // Trigger the browser's print functionality
+      onClose(); // Close the modal after printing (you can remove this if not needed)
+    }, 500);
+
+    return () => {
+      clearTimeout(printTimeout); // Clean up the timeout when the modal is unmounted
+    };
+  }, [onClose]);
+
   return (
     <div className="modal-container">
       <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">Order Invoice</h5>
-          <button className="close" onClick={onClose}>
-            &times;
-          </button>
-        </div>
         <div className="modal-body">
-          {/* Display the order details here */}
-          <p>Order ID: {order._id}</p>
-          <p>Full Name: {order.fullname}</p>
-          <p>Phone: {order.phone}</p>
-          <p>Date: {order.date}</p>
-          <p>Details: {order.details}</p>
-          <p>Category: {order.category}</p>
-          <p>Price: {order.price}</p>
-          <p>Payment Type: {order.paymentType}</p>
-          {/* Add more details as needed */}
+          {/* Display the order details for printing */}
+          <h4>Order Details:</h4>
+          <p>
+            <strong>Order ID:</strong> {order.orderId}
+          </p>
+          <p>
+            <strong>Full Name:</strong> {order.fullname}
+          </p>
+          <p>
+            <strong>Phone:</strong> {order.phone}
+          </p>
+          <p>
+            <strong>Date:</strong> {order.date}
+          </p>
+          <p>
+            <strong>Details:</strong> {order.details}
+          </p>
+          <p>
+            <strong>Category:</strong> {order.category}
+          </p>
+          <p>
+            <strong>Price:</strong> {order.price}
+          </p>
+          <p>
+            <strong>Payment Type:</strong> {order.paymentType}
+          </p>
         </div>
         <div className="modal-footer">
-          <button className="btn btn-primary" onClick={onClose}>
+          {/* Remove the print button, as printing will be triggered automatically */}
+          <button className="btn btn-secondary" onClick={onClose}>
             Close
           </button>
-          {/* You can add a print button here if needed */}
         </div>
       </div>
     </div>
