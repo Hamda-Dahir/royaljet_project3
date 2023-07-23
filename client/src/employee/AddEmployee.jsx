62ad9e3b4ae9df94ae9dd3bec854a210b07a3fc7
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-23 11:56:42 +0300

    added some function in add emp

--- client/src/employee/AddEmployee.jsx
+++ client/src/employee/AddEmployee.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createEmployee } from '../api';
+import { Form, Button } from 'react-bootstrap';
 
-function AddEmployee() {
+function AddEmployee({ onClose }) {
+  const navigate = useNavigate();
+  const [newEmployeeData, setNewEmployeeData] = useState({
+    name: '',
+    phone: 0,
+    position: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewEmployeeData((prevEmployeeData) => ({
+      ...prevEmployeeData,
+      [name]: value,
+    }));
+  };
+
+  const handleAddEmployee = async () => {
+    try {
+      await createEmployee(newEmployeeData);
+      onClose(); // Close the modal after adding a Employee
+      navigate('/employees');
+    } catch (error) {
+      console.error('Error creating employee:', error);
+    }
+  };
+
   return <div>AddEmployee</div>;
 }
 
