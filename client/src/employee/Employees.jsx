b79dbb474b9a0dc0b40ef1d206aa150fa751f066
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-23 11:38:35 +0300

    added import files

--- client/src/employee/Employees.jsx
+++ client/src/employee/Employees.jsx
@@ -1,4 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaFilter, FaFileAlt, FaPrint } from 'react-icons/fa';
+import EmployeeReport from './EmployeeReport';
+import './Employees.css';
+import {
+  getAllEmployees,
+  createEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from '../api';
+import AddEmployeeForm from './AddEmployee';
+import UpdateEmployeeForm from './UpdateEmployeeForm';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 function Employees() {
   return <div>Employees</div>;
