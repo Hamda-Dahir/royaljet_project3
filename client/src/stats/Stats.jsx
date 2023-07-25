df682b425744d817938cbacb899ea975f87cc684
Eng Abdikani Hamse <90378199+Eng-Shaab@users.noreply.github.com>
2023-07-25 09:47:20 +0300

    update stats and added more

--- client/src/stats/Stats.jsx
+++ client/src/stats/Stats.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
-import { FaUsers, FaShoppingCart, FaDollarSign } from 'react-icons/fa';
+import { FaUsers, FaShoppingCart, FaDollarSign, FaUser } from 'react-icons/fa';
 import './Stats.css';
 
-import { getAllUsers, getAllOrders, getAllExpenses } from '../api';
+import {
+  getAllUsers,
+  getAllOrders,
+  getAllExpenses,
+  getAllEmployees,
+} from '../api';
 
 const Stats = () => {
   const [usersCount, setUsersCount] = useState(0);
   const [ordersCount, setOrdersCount] = useState(0);
   const [expensesCount, setExpensesCount] = useState(0);
+  const [employeesCount, setEmployeesCount] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
@@ -16,9 +22,11 @@ const Stats = () => {
         const users = await getAllUsers();
         const orders = await getAllOrders();
         const expenses = await getAllExpenses();
+        const employees = await getAllEmployees();
         setUsersCount(users.length);
         setOrdersCount(orders.length);
         setExpensesCount(expenses.length);
+        setEmployeesCount(employees.length);
       } catch (error) {
         // handle error
       }
@@ -31,7 +39,7 @@ const Stats = () => {
     <div className="card-container d-flex justify-content-around flex-wrap">
       <Card className="custom-card users-card">
         <Card.Body>
-          <FaUsers className="icon" />
+          <FaUser className="icon" />
           <Card.Title>Users</Card.Title>
           <h3>{usersCount}</h3>
         </Card.Body>
@@ -48,6 +56,13 @@ const Stats = () => {
           <FaDollarSign className="icon" />
           <Card.Title>Expenses</Card.Title>
           <h3>{expensesCount}</h3>
+        </Card.Body>
+      </Card>
+      <Card className="custom-card employees-card">
+        <Card.Body>
+          <FaUsers className="icon" />
+          <Card.Title>Employee</Card.Title>
+          <h3>{employeesCount}</h3>
         </Card.Body>
       </Card>
     </div>
